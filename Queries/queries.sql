500e205ddf5c79feedd4a6f1d35f66d4a3bed978
Ananyag0210 <ananya.g@niveussolutions.com>
2022-12-20 15:16:59 +0530

    Assignment 2

---
+++ Queries/queries.sql
@@ -0,0 +1,7 @@
+INSERT INTO TABLE student(stu_name,branch_name) VALUES ('ananya','CSE');
+
+UPDATE student SET stu_name = 'Ananya G' WHERE std_id = 1;
+
+SELECT * FROM student;
+
+DELETE FROM student WHERE std_id = 1
